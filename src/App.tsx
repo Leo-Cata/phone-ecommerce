d103c80ce782f3195e6f20c34fb357576bb95161
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-07-12 17:10:13 -0300

    added routing for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import CardsContainer from './containers/CardsContainer'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router'
+import Latest from './components/Latest'
+import TopByInterest from './components/Navbar/TopByInterest'
+import TopByFans from './components/Navbar/TopByFans'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <CardsContainer />
+      <Routes>
+        <Route path="/" element={<CardsContainer />} />
+        <Route path="/latest" element={<Latest />} />
+        <Route path="/interest" element={<TopByInterest />} />
+        <Route path="/fans" element={<TopByFans />} />
+      </Routes>
     </div>
   )
 }
