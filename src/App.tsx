f3358d76c43d6acda9ac3b21deab7a64b76db8af
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-07-15 16:45:51 -0300

    dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import CardsContainer from './containers/CardsContainer'
+import BrandsCardsContainer from './containers/BrandsCardsContainer'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router'
 import Latest from './components/Latest'
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<CardsContainer />} />
+        <Route path="/" element={<BrandsCardsContainer />} />
         <Route path="/latest" element={<Latest />} />
         <Route path="/interest" element={<TopByInterest />} />
         <Route path="/fans" element={<TopByFans />} />
