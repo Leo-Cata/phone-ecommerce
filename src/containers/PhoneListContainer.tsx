cc938108c3d27f6e3b80e5a0654fba8381ec3242
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-07-17 17:35:49 -0300

    set page number

--- src/containers/PhoneListContainer.tsx
+++ src/containers/PhoneListContainer.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router'
 import { getBrandBySlug } from '../services/phoneApi'
 import { BrandsPhoneList, Phones } from '../types/types'
 import { AxiosResponse } from 'axios'
-import { Stack } from '@mui/material'
+import { Button, ButtonGroup, Stack } from '@mui/material'
 import PhoneCards from '../components/PhoneCards'
 
 const PhoneListContainer = () => {
@@ -15,7 +15,7 @@ const PhoneListContainer = () => {
 
   const [page, setPage] = useState(1)
 
-  const [pagesNumber, setPagesNumber] = useState(1)
+  const [numberOfPages, setNumberOfPages] = useState(1)
 
   //fetch the list of phones for said brand
   useEffect(() => {
@@ -26,7 +26,7 @@ const PhoneListContainer = () => {
             brandSlug,
             page,
           )
-          setPagesNumber(response.data.data.last_page)
+          setNumberOfPages(response.data.data.last_page)
           const brandsPhones: Phones[] = response.data.data.phones
           setBrandsPhones(brandsPhones)
         }
@@ -36,10 +36,27 @@ const PhoneListContainer = () => {
     }
     fetchBrandsPhones()
   }, [brandSlug, page])
+  console.log('number of pages: ' + numberOfPages)
+  const pageNumbers = Array.from(
+    { length: numberOfPages },
+    (_, index) => index + 1,
+  )
+  console.log(pageNumbers)
+
+  const handlePage = (page: number) => {
+    setPage(page)
+  }
 
   return (
-    <Stack className="h-full w-full px-40 py-10">
+    <Stack className="h-full w-full px-40 py-10" spacing={4}>
       {brandsPhones && <PhoneCards brandsPhones={brandsPhones} />}
+      <ButtonGroup className="flex justify-center">
+        {pageNumbers.map((number) => (
+          <Button key={number} onClick={() => handlePage(number)}>
+            {number}
+          </Button>
+        ))}
+      </ButtonGroup>
     </Stack>
   )
 }
