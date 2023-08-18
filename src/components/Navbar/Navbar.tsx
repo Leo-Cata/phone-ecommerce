3c3214b74e47b1c76a7db567c89dc64f740e4cf3
Leo <leonel.m.cata03@gmail.com>
2023-08-18 00:38:21 -0300

    fixed nav buttons not navigating outside of the text

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,10 +5,10 @@ import {
   Typography,
   Stack,
   Button,
-} from '@mui/material'
-import EdgesensorHighRoundedIcon from '@mui/icons-material/EdgesensorHighRounded'
-import NavbarDrawer from './NavbarDrawer'
-import { Link } from 'react-router-dom'
+} from "@mui/material";
+import EdgesensorHighRoundedIcon from "@mui/icons-material/EdgesensorHighRounded";
+import NavbarDrawer from "./NavbarDrawer";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -16,35 +16,37 @@ const Navbar = () => {
     <AppBar position="static" className="bg-custom-secondary">
       <Toolbar>
         {/* icon and app name starts */}
-        <IconButton
-          size="small"
-          edge="start"
-          aria-label="logo"
-          className="cursor-pointer text-custom-text">
-          <Link to={'/'}>
+        <Link to={"/"}>
+          <IconButton
+            size="small"
+            edge="start"
+            aria-label="logo"
+            className="cursor-pointer text-custom-text"
+          >
             <EdgesensorHighRoundedIcon />
-          </Link>
-        </IconButton>
+          </IconButton>
+        </Link>
         <Typography variant="h6" className="flex-grow">
-          <Link to={'/'}>Phone E-commerce</Link>
+          <Link to={"/"}>Phone E-commerce</Link>
         </Typography>
         {/* icon and app name ends */}
 
         {/* links buttons starts*/}
         <Stack
-          direction={'row'}
+          direction={"row"}
           spacing={2}
-          className="hidden text-white md:block">
-          <Button color="inherit" variant="text">
-            <Link to={'/'}>
+          className="hidden text-white md:block"
+        >
+          <Link to={"/"}>
+            <Button color="inherit" variant="text" className="">
               <Typography variant="subtitle1">Latest</Typography>
-            </Link>
-          </Button>
-          <Button color="inherit" variant="text">
-            <Link to={'/brands'}>
+            </Button>
+          </Link>
+          <Link to={"/brands"}>
+            <Button color="inherit" variant="text">
               <Typography variant="subtitle1">Phone Brands</Typography>
-            </Link>
-          </Button>
+            </Button>
+          </Link>
         </Stack>
         {/* links buttons ends*/}
 
@@ -52,7 +54,7 @@ const Navbar = () => {
         <NavbarDrawer />
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
