b01cbca44019810547b957244b658500c1cd658a
Leo <leonel.m.cata03@gmail.com>
2023-08-28 17:19:07 -0300

    wip search function

--- src/components/BrandsCards.tsx
+++ src/components/BrandsCards.tsx
@@ -1,5 +1,5 @@
 import { Grid } from "@mui/material";
-import { PhoneBrands } from "../types/types";
+import { BrandsImages, PhoneBrands } from "../types/types";
 import { useEffect, useState } from "react";
 import { getBrandBySlug } from "../services/phoneApi";
 import CustomCard from "./CustomCard";
@@ -31,21 +31,65 @@ const BrandsCards = ({ phoneBrands }: { phoneBrands: PhoneBrands[] }) => {
     fetchBrandsImgBySlug();
   }, [phoneBrands]);
 
+  // code to manage searching for specific brands
+
+  //state to save the word to search for
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  //filters the phones brands returning only matches
+  const filteredBrands = phoneBrands.filter((brand) =>
+    brand.brand_name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  //empty array to save imgs to keys that are equal to the brands names
+  const brandsImages: BrandsImages = {};
+
+  //if there are imgs, saves a key equal to the brand name and an img according to the index
+  if (phoneImages) {
+    phoneBrands.forEach((brand, index) => {
+      brandsImages[brand.brand_name] = phoneImages[index];
+    });
+  }
+
+  // code to manage searching for specific brands ends
   return (
     <Grid container>
-      {/* if there are images, map through each of phone brands, else display a simple skeleton */}
+      <input
+        type="text"
+        placeholder="asd"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="text-blue-900"
+      />
+      {/* if there are images, map through each of phone brands, else display a simple skeleton
+      when the user types in the search box, it will filter and render only the matches, if the query becomes empty it will show all brands
+      */}
       {phoneImages ? (
-        phoneBrands.map((brand, index) => (
-          <CustomCard
-            key={brand.brand_slug}
-            customKey={brand.brand_slug}
-            linkTo={`/brands/${brand.brand_slug}`}
-            image={phoneImages[index]}
-            alt={`${brand.brand_name}`}
-            title={brand.brand_name}
-            text={`Available Devices ${brand.device_count}`}
-          />
-        ))
+        searchQuery === "" ? (
+          phoneBrands.map((brand) => (
+            <CustomCard
+              key={brand.brand_slug}
+              customKey={brand.brand_slug}
+              linkTo={`/brands/${brand.brand_slug}`}
+              image={brandsImages[brand.brand_name]}
+              alt={`${brand.brand_name}`}
+              title={brand.brand_name}
+              text={`Available Devices ${brand.device_count}`}
+            />
+          ))
+        ) : (
+          filteredBrands.map((brand) => (
+            <CustomCard
+              key={brand.brand_slug}
+              customKey={brand.brand_slug}
+              linkTo={`/brands/${brand.brand_slug}`}
+              image={brandsImages[brand.brand_name]}
+              alt={`${brand.brand_name}`}
+              title={brand.brand_name}
+              text={`Available Devices ${brand.device_count}`}
+            />
+          ))
+        )
       ) : (
         <CustomCardsSkeleton />
       )}
